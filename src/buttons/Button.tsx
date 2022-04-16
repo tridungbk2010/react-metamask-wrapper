b3e8ad3b78c846944bca5c78cf3ae131b8a9ead8
Chris Ho <chris.tridung@gmail.com>
2022-04-16 09:35:05 +0700

    Add build tooling

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,5 +1,10 @@
-const Button = () => {
-  return <div>button</div>;
+import { FC, ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+export const Button: FC<Props> = ({ children }) => {
+  return <button>{children}</button>;
 };
 
 export default Button;
